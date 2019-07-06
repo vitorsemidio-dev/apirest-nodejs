7e6fe9c75f1b974708d9bc3337ccdbc1a47b31c3
Vitor Emidio <vitorsemidio@gmail.com>
2019-07-06 17:41:32 -0300

    Registro sem retornar a senha e fim da aula 1

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -13,6 +13,8 @@ router.post('/register', async (req, res) => {
 
         const user = await User.create(req.body);
 
+        user.password = undefined;
+
         return res.send(user);
     } catch (err) {
         return res.status(400).send({ error: 'Registration failed' });
