c35ca73f7d1bb79cde9ee8298213f44245f0ec50
Vitor Emidio <vitorsemidio@gmail.com>
2019-07-06 17:37:34 -0300

    Usuario jah existente e nodemon

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,10 +5,15 @@ const User = require('../models/User');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+    const { email } = req.body;
     try {
+        if (await User.findOne({ email })) {
+            return res.status(400).send({ error: 'User already exists' })
+        }
+
         const user = await User.create(req.body);
 
-        return res.send({ user });
+        return res.send(user);
     } catch (err) {
         return res.status(400).send({ error: 'Registration failed' });
     }
