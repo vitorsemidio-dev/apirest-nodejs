8f871da9c709e171f59eaca809fbcf5abb6e452e
Vitor Emidio <vitorsemidio@gmail.com>
2019-07-09 20:56:47 -0300

    Modelo de Project e Task desfinidos

---
+++ src/app/models/Task.js
@@ -0,0 +1,34 @@
+const mongoose = require('../../database/index');
+const bcrypt = require('bcryptjs');
+
+const TaskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Project',
+        required: true,
+    },
+    assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    completed: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    createAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+
+const Task = mongoose.model('Task', TaskSchema);
+
+
+module.exports = Task;
