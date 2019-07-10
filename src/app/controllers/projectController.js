4aa57d425c39d3fff0f2b64170846569d47b9333
Vitor Emidio <vitorsemidio@gmail.com>
2019-07-09 21:12:54 -0300

    esqueleto CRUD ProjectController

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/auth');
 
+const Project = require('../models/Project');
+const Task = require('../models/Task');
+
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.get('/', (req, res) => {
-    res.send({ ok: true, user: req.userId });
+router.get('/', async(req, res) => {
+    res.send({ ok: true, user: req.userId, get: 'all' });
+});
+
+router.get('/:projectId', async(req, res) => {
+    res.send({ ok: true, user: req.userId, get: 'id' });
+});
+
+router.put('/:projectId', async(req, res) => {
+    res.send({ ok: true, user: req.userId, put: 'put' });
+});
+
+router.post('/', async(req, res) => {
+    res.send({ ok: true, user: req.userId , post: 'post' });
+});
+
+router.delete('/:projectId', async(req, res) => {
+    res.send({ ok: true, user: req.userId, delete: 'delete' });
 });
 
 
